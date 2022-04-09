08110e14f6396914e88c28adbe6ef8ec066f1e75
大野幹人 <mbp@oonomikihitonoMacBook-Pro.local>
2022-04-09 16:39:20 +0900

    prepared the development environment

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  devServer: {
+    proxy: "http://localhost:3030",
+    port: 3000,
+  },
   transpileDependencies: ["vuetify"],
 });
