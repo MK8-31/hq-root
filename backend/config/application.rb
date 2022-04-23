1d5c57be72af75501475f2e0750ee5a6e8fef208
大野幹人 <mbp@oonomikihitonoMacBook-Pro.local>
2022-04-23 10:08:21 +0900

    finish creating signup and login in rails

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -36,5 +36,21 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # added
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore,
+                          config.session_options
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 expose: %w[access-token expiry token-type uid client],
+                 methods: %i[get post options delete put]
+      end
+    end
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   namespace :api do
     namespace :v1 do
       resources :tasks, only: :index
       get :health_check, to: 'health_check#index'
+      mount_devise_token_auth_for 'User',
+                                  at: 'auth',
+                                  controllers: {
+                                    registrations: 'api/v1/auth/registrations',
+                                  }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
